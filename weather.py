e808d57f0d4e2d2923edf3b75e2621e65476a499
evgeny1337 <genya-sozykin@mail.ru>
2024-10-17 23:13:16 +0500

    Work_on_mistakes_1

--- weather.py
+++ weather.py
@@ -1,10 +1,15 @@
 import requests
 
-places = ['Череповец', 'Шереметьево', 'Лондон']
-for place in places:
-    url = f'http://wttr.in/{place}?nTquM&lang=ru'
-    response = requests.get(url)
-    if not response.ok:
-        print(requests.exceptions.HTTPError(response=response))
-    else:
+def main():
+    places = ['Череповец', 'svo', 'Лондон']
+    params = {'n':'','T':'','q':'','M':'','lang':'ru'}
+    for place in places:
+        url = f'http://wttr.in/{place}'
+        yield [url,params]
+
+if __name__ == '__main__':
+    result = main()
+    for item in result:
+        response = requests.get(url=item[0],params=item[1])
+        response.raise_for_status()
         print(response.text)
