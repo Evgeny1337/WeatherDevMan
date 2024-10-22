78e55049a7bbc4679d665994b3cb9776fa3f8f6d
DirectumRXUser <directumrxuser@c.ru>
2024-10-22 11:47:48 +0300

    Added status checking

--- weather.py
+++ weather.py
@@ -1,19 +1,18 @@
 import requests
 
-def get_urls():
-    places = ['Череповец', 'svo', 'Лондон']
-    for place in places:
-        yield f'http://wttr.in/{place}'
-
 def main():
     params = {'n':'','T':'','q':'','M':'','lang':'ru'}
-    urls = get_urls()
-    for url in urls:
-        response = requests.get(url=url,params=params)
-        yield response.text
+    places = ['Череповец', 'svo', 'Лондон']
+    for place in places:
+        url = f'http://wttr.in/{place}'
+        try:
+            response = requests.get(url=url,params=params)
+            response.raise_for_status()
+            print(response.text)
+        except requests.exceptions.HTTPError as err:
+            print('Response is: {content}'.format(content=err.response.text))
 
+            
 if __name__ == '__main__':
-    responses = main()
-    for response in responses:
-        print(response)
+    main()
 
